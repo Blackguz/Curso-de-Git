34608e96e6820d11523c062aefde7f60ec650b8c
Blackguz <roman8787.gv@gmail.com>
2023-03-09 16:40:28 -0600

    Se agrego la funcion para llorar, se modifico la funcion de despedir

--- main.java
+++ main.java
@@ -6,6 +6,11 @@ public class main{
 
     static void despedir(){
         System.out.println("Adios mundo");
+        System.out.println("Fue bonito mientras duro");
+    }
+
+    static void llorar(){
+        System.out.println("Llorar");
     }
     public static void main(String[] args){
         saludar();
