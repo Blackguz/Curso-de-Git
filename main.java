ff026d7fec19c150490da610c0b06d244e1e9c73
Blackguz <roman8787.gv@gmail.com>
2023-03-09 16:28:57 -0600

    Se a creado la funcion para despedir

--- main.java
+++ main.java
@@ -3,7 +3,12 @@ public class main{
     static void saludar(){
         System.out.println("Hola mundo");
     }
+
+    static void despedir(){
+        System.out.println("Adios mundo");
+    }
     public static void main(String[] args){
         saludar();
+        despedir();
     }
 }
