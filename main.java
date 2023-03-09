521a393654a835ecd4e836369a51de83333c382f
Blackguz <roman8787.gv@gmail.com>
2023-03-09 16:37:33 -0600

    Se creo la funcion Sumar, para dos numeros enteros

--- main.java
+++ main.java
@@ -7,8 +7,13 @@ public class main{
     static void despedir(){
         System.out.println("Adios mundo");
     }
+
+    static int sumar(int a, int b){
+        return a + b;
+    }
     public static void main(String[] args){
         saludar();
         despedir();
+        System.out.println(sumar(5, 6));
     }
 }
